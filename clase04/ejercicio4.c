bcadd9e399783836ef05a028d4d9c721944dc3ca
JuanHerencia <juanluis.herencia@gmail.com>
2026-04-13 22:01:15 -0500

    Ejercicios 4,5 modificados

--- clase04/ejercicio4.c
+++ clase04/ejercicio4.c
@@ -2,22 +2,29 @@
 // switch y enum
 #include <stdio.h>
 
+// DiaSemana es un tipo de variable enum
+// Por defecto LUNES es 0, MARTES es 1, ...
 typedef enum { LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO } DiaSemana;
+typedef enum { MASCULINO, FEMENINO } Sexo; // MASCULINO es 0, FEMENINO es 1
+typedef enum { ARRIBA, ABAJO, DERECHA, IZQUIERDA} Tecla_flecha;
 
+// función que devuelve un puntero a una cadena
+// devuelve la dirección de meoria de un texto
 const char* tipo_dia(DiaSemana d) {
-    switch (d) {
-        case SABADO:
-        case DOMINGO:
-            return "Fin de semana";
-        case LUNES:
-            return "Inicio laboral";
-        default:
-            return "Día laboral";
+    // d es un valor constante
+    switch (d) { // selector se ejecuta por vez uno o mas casos y sale de switch
+        case SABADO:  // caso 5
+        case DOMINGO: // caso 6
+            return "Fin de semana"; // de cada opción se sale (termina) con return
+        case LUNES:   // caso 0
+            return "Inicio laboral"; // normalmente se usa break para salir de un switch
+        default:      // Cuando no se cumpla ninguna de las anteriores, considerar esta opción
+            return "Dia laboral";
     }
 }
 
 int main() {
     for (int i = LUNES; i <= DOMINGO; i++)
-        printf("%d: %s\n", i, tipo_dia((DiaSemana)i));
+        printf("valor constante %d: %s\n", i, tipo_dia((DiaSemana)i));
     return 0;
 }
--- clase04/ejercicio5.c
+++ clase04/ejercicio5.c
@@ -5,15 +5,15 @@
 
 int main() {
     int matriz[5][5];
-    bool encontrado = false;
-    int valor_buscar = 42;
+    int valor_buscar = 12; //42;
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             matriz[i][j] = i * j;  // Relleno de ejemplo
             if (matriz[i][j] == valor_buscar) {
                 printf("Encontrado en [%d][%d]\n", i, j);
-                goto salir;
+                // Si se encuentra, ya no continuar con la búsqueda
+                goto salir;  // Mover la ejecución a la linea 22
             }
         }
     }
