e98298a77d0257865029306e8b43ed932eacb49b
Juan Herencia <129803977+JuanHerencia@users.noreply.github.com>
2026-05-04 20:23:29 -0500

    Se recupera la clase01

---
+++ clase01_recup/saludos.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int nro_arg, char *arg[]) {
+
+     printf("Bienvenido ");
+     printf("%s",arg[1]);
+     printf(" a Lenguaje de Programacion I\n");
+     printf("La clave es estudiar");
+    return 0;
+}
